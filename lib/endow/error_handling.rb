e886fc28f083f41040bc95f3eea50994ae0f5be8
Jason Harrelson <jason@lookforwardenterprises.com>
2014-04-07 10:11:41 -0500

    Handle subclasses of gracefully handled errors.

--- lib/endow/error_handling.rb
+++ lib/endow/error_handling.rb
@@ -7,6 +7,18 @@ module Endow
       graceful_errors
     end
 
+    def graceful_error_for( error )
+      if ( graceful_error = graceful_errors_map[error])
+        return graceful_error
+      end
+
+      graceful_errors_map.each do |klass, graceful_error|
+        return graceful_error if error.kind_of?( klass )
+      end
+
+      raise "Unexpected problem while attempting graceful handling of error: #{error.class.name} #{error.message}"
+    end
+
     def graceful_errors
       graceful_errors_map.keys
     end
@@ -18,11 +30,10 @@ module Endow
     def with_graceful_error_handling( &block )
       block.call
     rescue *graceful_errors => e
-      msg = "#{self.class.name}: #{graceful_errors_map[e.class][:msg]}"
+      graceful_error = graceful_error_for( e )
+      msg = "#{self.class.name}: #{graceful_error[:msg]}"
       log_graceful_error( msg )
-      raise graceful_errors_map[e.class][:klass],
-            msg,
-            e.backtrace
+      raise graceful_error[:klass], msg, e.backtrace
     end
 
   end
